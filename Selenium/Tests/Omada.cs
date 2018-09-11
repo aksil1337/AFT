31e2068c21cdeded6069b7296b44f6123c2f579e
Adrian Nowacki <adrian@nowacki.me>
2018-09-11 23:57:38 +0200

    Selenium (Code, Config)
    - Added Step 1 of Omada E2E test
    - Added Selenium and NUnit packages
    - Added Omada test class

---
+++ Selenium/Tests/Omada.cs
@@ -0,0 +1,37 @@
+﻿// Copyright © Adrian Nowacki. All rights reserved.
+// Licensed under the MIT License.
+
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Selenium.Tests
+{
+    [TestFixture]
+    class Omada
+    {
+        protected RemoteWebDriver Driver { get; private set; }
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            Driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            Driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+        }
+
+        [TearDown]
+        public void AfterEach()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void EndToEnd()
+        {
+            Driver.Navigate().GoToUrl("https://www.omada.net/");
+        }
+    }
+}
